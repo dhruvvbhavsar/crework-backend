c5c0bc8560377606be1de598f155666ae5f8cf36
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2024-07-31 18:51:05 +0530

    test

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
+app.get('/', (req, res) => {
+  res.send('Hello, workin!');
+});
 app.use('/api/auth', authRoutes);
 app.use("/api/tasks", taskRoutes);
 
