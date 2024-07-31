dc8130cb4b0b254a428266abfe0dc9cf75730903
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2024-07-31 19:14:28 +0530

    it worked

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
-const taskRoutes = require('./routes/task');
+const authRoutes = require('../routes/auth');
+const taskRoutes = require('../routes/task');
 
 const app = express();
 
@@ -27,3 +27,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
